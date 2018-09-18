c9481149c44ea7262df295a22f99cc4c318076bf
Md. Mamunur Roshid <mamunur.rashid@dnet.org.bd>
2018-09-18 11:16:19 +0600

    angular material and reports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, MdMenuModule, MdSidenavModule } from '@angular/material';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -152,8 +154,45 @@ import {RemittanceWelfareFundUpdateComponent} from './finance/remittance-welfare
 import  { MonthWiseViewService } from './month-wise-view/month-wise-view.service';
 import {MonthWiseViewComponent} from './month-wise-view/month-wise-view.component';
 import { CompanyReportComponent } from './reports/visit/company/company-report.component';
-import { GoogleChartComponent } from './google-chart/google-chart.component'
-import { ApiProcessService } from './api-process.service'
+import { GoogleChartComponent } from './google-chart/google-chart.component';
+import { ApiProcessService } from './api-process.service';
+import { MdCardModule, MdDatepickerModule, MdInputModule, MdNativeDateModule } from '@angular/material';
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,
+} from '@angular/material';
+import { CompatibilityModule } from '@angular/material';
+import {NoConflictStyleCompatibilityMode} from '@angular/material';
+import{MatDateFormats, MAT_DATE_FORMATS, NativeDateAdapter, DateAdapter} from '@angular/material';
 
 const appRoutes: Routes  = [
   {
@@ -651,6 +690,20 @@ const appRoutes: Routes  = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', component: LoginComponent }
 ];
+
+const MY_DATE_FORMATS = {
+    parse: {
+        dateInput: { day: 'numeric', month: 'numeric', year: 'numeric' }
+    },
+    display: {
+        dateInput: 'input',
+        monthYearLabel: { year: 'numeric', month: 'short' },
+        dateA11yLabel: { year: 'numeric', month: 'long', day: 'numeric' },
+        monthYearA11yLabel: { year: 'numeric', month: 'long' },
+    }
+ };
+
+
 // arbitration-disputes
 @NgModule({
   declarations: [
@@ -768,7 +821,48 @@ const appRoutes: Routes  = [
     HttpClientModule,
     BrowserModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdMenuModule,
+    MdSidenavModule,
+    MdDatepickerModule,
+    MdInputModule,
+    MdNativeDateModule,
+    MdCardModule,
+      MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,
+  CompatibilityModule,
+  NoConflictStyleCompatibilityMode,
   ],
   providers: [
     UserListService,
@@ -812,6 +906,7 @@ const appRoutes: Routes  = [
     RemittanceWelfareFundService,
     MonthWiseViewService,
     ApiProcessService,
+
   ],
   bootstrap: [AppComponent]
 })
--- src/app/reports/visit/company/company-report.component.ts
+++ src/app/reports/visit/company/company-report.component.ts
@@ -3,6 +3,7 @@ import {GoogleChartComponent} from '../../../google-chart/google-chart.component
 import { ApiProcessService } from '../../../api-process.service';
 import {AuthenticationService} from '../../../authentication.service';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+
 declare var $: any;
 
 @Component({
@@ -13,128 +14,34 @@ export class CompanyReportComponent implements OnInit {
   authorizationKey;
   countryListResponse;
   labourattacheListResponse: any;
+  roomsFilter;
+  getData;
+  company_VisitData;
+  company_VisitOptions;
+  jail_VisitData;
+  jail_VisitOptions;
+  hostpital_VisitData;
+  hostpital_VisitOptions;
+  migrationshelter_VisitData;
+  migrationshelter_VisitOptions;
+  remittance_FincaneData;
+  remittance_FincaneOptions;
+  budget_FincaneData;
+  budget_FincaneOptions;
+  marketassessment_ActivityData;
+  marketassessment_ActivityOptions;
+  employeeenhancement_ActivityData;
+  employeeenhancement_ActivityOptions;
+  CompanyReportResponse;
 
   constructor(private _apiProcessService: ApiProcessService,
               private _authentication: AuthenticationService,
-              private _http: HttpClient) { }
+              private _http: HttpClient) {
 
-
-  /* Module Visit */
-      /*
-      * Company Visit
-      */
-      public company_VisitData = [
-        ['Task', 'Hours per Day'],
-        ['Work',     11],
-        ['Eat',      2],
-        ['Commute',  2],
-        ['Watch TV', 2],
-        ['Sleep',    7] ];
-      public company_VisitOptions  = {
-        title: 'Company Visit ',
-      };
-
-      /*
-      * Jail Visit
-      */
-      public jail_VisitData = [
-        ['Task', 'Hours per Day'],
-        ['Work',     11],
-        ['Eat',      2],
-        ['Commute',  2],
-        ['Watch TV', 2],
-        ['Sleep',    7] ];
-      public jail_VisitOptions  = {
-        title: 'Jail Visit',
-      };
-
-      /*
-      * Hostpital Visit
-      */
-      public hostpital_VisitData = [
-        ['Task', 'Hours per Day'],
-        ['Work',     11],
-        ['Eat',      2],
-        ['Commute',  2],
-        ['Watch TV', 2],
-        ['Sleep',    7] ];
-      public hostpital_VisitOptions  = {
-        title: 'Hostpital Visit ',
-      };
-
-      /*
-      * Migration Shelter Visit
-      */
-      public migrationshelter_VisitData = [
-        ['Task', 'Hours per Day'],
-        ['Work',     11],
-        ['Eat',      2],
-        ['Commute',  2],
-        ['Watch TV', 2],
-        ['Sleep',    7] ];
-      public migrationshelter_VisitOptions  = {
-        title: 'Migration Shelter Visit',
-      };
-
-  /* Module Finance */
-      /*
-      * Remittance & WelfareFund
-      */
-      public remittance_FincaneData = [
-        ['Task', 'Hours per Day'],
-        ['Work',     11],
-        ['Eat',      2],
-        ['Commute',  2],
-        ['Watch TV', 2],
-        ['Sleep',    7] ];
-      public remittance_FincaneOptions  = {
-        title: 'Remittance & WelfareFund',
-      };
-
-      /*
-      * Budget
-      */
-      public budget_FincaneData = [
-        ['Task', 'Hours per Day'],
-        ['Work',     11],
-        ['Eat',      2],
-        ['Commute',  2],
-        ['Watch TV', 2],
-        ['Sleep',    7] ];
-      public budget_FincaneOptions  = {
-        title: 'Jail Visit',
-      };
-  /* Module Activity */
-      /*
-      * Market Assessment
-      */
-      public marketassessment_ActivityData = [
-        ['Task', 'Hours per Day'],
-        ['Work',     11],
-        ['Eat',      2],
-        ['Commute',  2],
-        ['Watch TV', 2],
-        ['Sleep',    7] ];
-      public marketassessment_ActivityOptions  = {
-        title: 'Market Assessment Activity ',
-      };
-
-      /*
-      * Employee Enhancement
-      */
-      public employeeenhancement_ActivityData = [
-        ['Task', 'Hours per Day'],
-        ['Work',     11],
-        ['Eat',      2],
-        ['Commute',  2],
-        ['Watch TV', 2],
-        ['Sleep',    7] ];
-      public employeeenhancement_ActivityOptions  = {
-        title: 'Employee Enhancement',
-      };
+               }
 
   ngOnInit() {
-      // to solve the left menu hide problem;
+        // to solve the left menu hide problem;
       $(document).ready(() => {
       const trees: any = $('[data-widget="tree"]');
       trees.tree();
@@ -151,5 +58,135 @@ export class CompanyReportComponent implements OnInit {
       this.labourattacheListResponse = response;
       });
 
+      // get company reports;
+      this._apiProcessService.getListData(this.authorizationKey, 'user/labourattache').subscribe( response => {
+      this.CompanyReportResponse = response;
+      });
+
+       /* Module Visit */
+      /*
+      * Company Visit
+      */
+
+      this.company_VisitData = [
+      ['Task', 'Hours per Day'],
+        ['Work',     11],
+        ['Eat',      2],
+        ['Commute',  2],
+        ['Watch TV', 2],
+        ['Sleep',    7] ];
+        
+      this.company_VisitOptions  = {
+        title: 'Company Visit ',
+      };
+
+      /*
+      * Jail Visit
+      */
+       this.jail_VisitData = [
+        ['Task', 'Hours per Day'],
+        ['Work',     11],
+        ['Eat',      2],
+        ['Commute',  2],
+        ['Watch TV', 2],
+        ['Sleep',    7] ];
+      this.jail_VisitOptions  = {
+        title: 'Jail Visit',
+      };
+
+      /*
+      * Hostpital Visit
+      */
+      this.hostpital_VisitData = [
+        ['Task', 'Hours per Day'],
+        ['Work',     11],
+        ['Eat',      2],
+        ['Commute',  2],
+        ['Watch TV', 2],
+        ['Sleep',    7] ];
+      this.hostpital_VisitOptions  = {
+        title: 'Hostpital Visit ',
+      };
+
+      /*
+      * Migration Shelter Visit
+      */
+      this.migrationshelter_VisitData = [
+        ['Task', 'Hours per Day'],
+        ['Work',     11],
+        ['Eat',      2],
+        ['Commute',  2],
+        ['Watch TV', 2],
+        ['Sleep',    7] ];
+      this.migrationshelter_VisitOptions  = {
+        title: 'Migration Shelter Visit',
+      };
+
+      /* Module Finance */
+      /*
+      * Remittance & WelfareFund
+      */
+      this.remittance_FincaneData = [
+        ['Task', 'Hours per Day'],
+        ['Work',     11],
+        ['Eat',      2],
+        ['Commute',  2],
+        ['Watch TV', 2],
+        ['Sleep',    7] ];
+      this.remittance_FincaneOptions  = {
+        title: 'Remittance & WelfareFund',
+      };
+
+      /*
+      * Budget
+      */
+      this.budget_FincaneData = [
+        ['Task', 'Hours per Day'],
+        ['Work',     11],
+        ['Eat',      2],
+        ['Commute',  2],
+        ['Watch TV', 2],
+        ['Sleep',    7] ];
+
+      this.budget_FincaneOptions  = {
+        title: 'Jail Visit',
+      };
+  /* Module Activity */
+      /*
+      * Market Assessment
+      */
+      this.marketassessment_ActivityData = [
+        ['Task', 'Hours per Day'],
+        ['Work',     11],
+        ['Eat',      2],
+        ['Commute',  2],
+        ['Watch TV', 2],
+        ['Sleep',    7] ];
+      this.marketassessment_ActivityOptions  = {
+        title: 'Market Assessment Activity ',
+      };
+
+      /*
+      * Employee Enhancement
+      */
+      this.employeeenhancement_ActivityData = [
+        ['Task', 'Hours per Day'],
+        ['Work',     11],
+        ['Eat',      2],
+        ['Commute',  2],
+        ['Watch TV', 2],
+        ['Sleep',    7] ];
+      this.employeeenhancement_ActivityOptions  = {
+        title: 'Employee Enhancement',
+      };
+
+
+
+
+
+
+
+
    }
+
 }
