fd48155f183ac640fd1c58e657b5d8091f83f0b6
softactor <tanveerqureshee1@gmail.com>
2018-09-23 08:24:54 +0600

    working on assistance provide

---
+++ src/app/assistance-provide/general-assistance/general-assistance.model.ts
@@ -0,0 +1,7 @@
+export class GeneralAssistanceModel {
+  name:   string;
+  address:  string;
+  issue:           string;
+  action_taken:           string;
+  type:           string;
+}
--- src/app/resolved/remit-fc/remit-fc-list.component.ts
+++ src/app/resolved/remit-fc/remit-fc-list.component.ts
@@ -17,6 +17,9 @@ export class RemitFcListComponent implements OnInit {
   feedbackData: any;
   tableFeedbackData;
   responseError;
+  defaultDate;
+  assignTo;
+  listApi;
   constructor(
     private _toasterService: TosterService,
     private _authentication: AuthenticationService,
@@ -29,11 +32,19 @@ export class RemitFcListComponent implements OnInit {
         $('#data_list').DataTable({
           'lengthMenu': [[25, 50, -1], [25, 50, 'All']]
         });
-      }
-    });
+        }
+        $('#defaultDate').datepicker({
+          dateFormat: 'yy-mm'
+        });
+        $('#defaultDate').datepicker('setDate', new Date());
+      });
     }, 1000);
-    this.authorizationKey = localStorage.getItem('token_type') + ' ' + localStorage.getItem('access_token');
-    this._service.getListData(this.authorizationKey, 'resolved/remitfc/list/').subscribe( response => {
+    this.assignTo = localStorage.getItem('assign_to');
+    // this.defaultDate  = $('#defaultDate').val();
+    this.defaultDate        =   new Date();
+    this.authorizationKey   =   localStorage.getItem('token_type') + ' ' + localStorage.getItem('access_token');
+    this.listApi            = 'resolved/remitfc/list?type=Remit fc';
+    this._service.getListData(this.authorizationKey, this.listApi).subscribe( response => {
         this.tableListData = response;
         this.feedbackData = this.tableListData.results;
       },
@@ -58,7 +69,7 @@ export class RemitFcListComponent implements OnInit {
     this._service.delete(deleteParam, 'resolved/remitfc/delete/').subscribe( response => {
       this.tableDeleteData = response;
       this._toasterService.success('Data have been successfully deleted.');
-      this._service.getListData(this.authorizationKey, 'resolved/remitfc/list/').subscribe( listResponse => {
+      this._service.getListData(this.authorizationKey, this.listApi).subscribe( listResponse => {
           this.tableListData = listResponse;
           this.feedbackData = this.tableListData.results;
         },
