06e9e03fff4e0a0dcaab3e8f46cea9c61df548f5
softactor <tanveerqureshee1@gmail.com>
2018-09-30 09:47:30 +0600

    chart dynamic data problem solve

--- src/app/reports/visit/visit-reports.component.ts
+++ src/app/reports/visit/visit-reports.component.ts
@@ -18,76 +18,9 @@ export class VisitReportsComponent implements OnInit {
   authorizationKey;
   visitReportData;
   // Pie
-  public pieChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
+  public pieChartLabels: string[] = ['Bangladesh', 'India', 'Pakisthan'];
   public pieChartData: number[] = [300, 500, 100];
   public pieChartType: string = 'pie';
-  // lineChart
-  lineChartData: Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
-  ];
-  public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartOptions: any = {
-    responsive: true
-  };
-  public lineChartColors: Array<any> = [
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    },
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
-    },
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    }
-  ];
-  public lineChartLegend: boolean = true;
-  public lineChartType: string = 'line';
-  public randomize(): void {
-    const _lineChartData: Array<any> = new Array(this.lineChartData.length);
-    for (let i = 0; i < this.lineChartData.length; i++) {
-      _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-        _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-      }
-    }
-    this.lineChartData = _lineChartData;
-    this._apiProcessService.getListData(this.authorizationKey, 'visit/company/report').subscribe( response => {
-      this.visitReportData = response;
-      const pieChartsLevelList: Array<any> = [];
-      const pieChartDataList: Array<any> = [];
-      pieChartsLevelList.push('India');
-      pieChartDataList.push(45);
-      for (const testData of this.visitReportData) {
-        pieChartsLevelList.push(testData.assign_to__country_name);
-        pieChartDataList.push(testData.total);
-      } // end of for
-      console.log(this.pieChartLabels);
-      console.log(this.pieChartData);
-      this.pieChartLabels = pieChartsLevelList;
-      this.pieChartData = pieChartDataList;
-      console.log(this.pieChartLabels);
-      console.log(this.pieChartData);
-      this.refresh_chart();
-    });
-  }
-
   // events
   public chartClicked(e: any): void {
     console.log(e);
@@ -119,11 +52,23 @@ export class VisitReportsComponent implements OnInit {
     // @ts-ignore
     this.assignTo = localStorage.getItem('assign_to');
     this.authorizationKey = localStorage.getItem('token_type') + ' ' + localStorage.getItem('access_token');
+  }  
+  public randomize(): void {
+    this._apiProcessService.getListData(this.authorizationKey, 'visit/company/report').subscribe( response => {
+      this.visitReportData = response;
+      this.pieChartLabels.length = 0;
+      this.pieChartLabels  =  [];
+      this.pieChartData  =  [];
+      for (const testData of this.visitReportData) {
+        this.pieChartLabels.push(testData.assign_to__country_name);
+        this.pieChartData.push(testData.total);
+      } // end of for
+      this.refresh_chart();
+    });
   }
   refresh_chart() {
-    console.log('show refresh');
     setTimeout(() => {
       this._chartRef.refresh();
-    }, 10);
+    }, 100);
   }
 }
