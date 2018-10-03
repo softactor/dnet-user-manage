19aeef7a8f84d4bdc14cde10b16339b141bc26db
Md. Mamunur Roshid <mamunur.rashid@dnet.org.bd>
2018-10-03 16:47:33 +0600

    report chart

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@
 export const environment = {
   production: false,
   // baseApi  : 'http://192.168.3.70:8000/api/'
- baseApi  : 'http://labourattache.com.bd/api/'
-  // baseApi  : 'http://192.168.2.178:8000/api/'
+ //baseApi  : 'http://labourattache.com.bd/api/'
+ baseApi  : 'http://192.168.2.178:8000/api/'
 };
